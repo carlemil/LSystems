0b0a23e949388a1baea994106733fdc272cb267a
Carl-Emil Kjellstrand <erbnsman@gmail.com>
2021-02-05 23:57:10 +0100

    Reduce the amoung of GC needed for consecutive calls to render the same system with a new image.

--- src/main/kotlin/se/kjellstrand/lsystem/VariableWidthCurve.kt
+++ src/main/kotlin/se/kjellstrand/lsystem/VariableWidthCurve.kt
@@ -3,11 +3,22 @@ package se.kjellstrand.lsystem
 import se.kjellstrand.lsystem.model.LSTriple
 import kotlin.math.*
 
-fun buildHullFromPolygon(ppList: List<LSTriple>): MutableList<LSTriple> {
-    val leftHull = mutableListOf<LSTriple>()
-    val rightHull = mutableListOf<LSTriple>()
+var leftHull = mutableListOf<LSTriple>()
+var rightHull = mutableListOf<LSTriple>()
 
-    for (i in 1 until ppList.size) {
+fun buildHullFromPolygon(ppList: List<LSTriple>): MutableList<LSTriple> {
+    // Only create new left and right hulls if(leftHull.size != ppList.size -1)
+    if (leftHull.size != ppList.size - 1) {
+        leftHull = mutableListOf()
+        rightHull = mutableListOf()
+        // Initialize the left and right hull lists
+        for (i in 0 until ppList.size-2) {
+            leftHull.add(LSTriple(0f, 0f, 1f))
+            rightHull.add(LSTriple(0f, 0f, 1f))
+        }
+    }
+
+    for (i in 1 until ppList.size - 1) {
         val p0 = ppList[i - 1]
         val p1 = ppList[i]
 
@@ -16,8 +27,8 @@ fun buildHullFromPolygon(ppList: List<LSTriple>): MutableList<LSTriple> {
         val alfaPlus90 = calculateAlfa(a, c, b, (PI / 2.0).toFloat())
         val alfaMinus90 = calculateAlfa(a, c, b, -(PI / 2.0).toFloat())
 
-        leftHull.add(calculatePerpendicularPolyPoint(p0, p1, alfaPlus90))
-        rightHull.add(calculatePerpendicularPolyPoint(p0, p1, alfaMinus90))
+        calculatePerpendicularPolyPoint(leftHull[i - 1], p0, p1, alfaPlus90)
+        calculatePerpendicularPolyPoint(rightHull[i - 1], p0, p1, alfaMinus90)
     }
 
     val hull = mutableListOf<LSTriple>()
@@ -27,20 +38,22 @@ fun buildHullFromPolygon(ppList: List<LSTriple>): MutableList<LSTriple> {
 }
 
 private fun calculatePerpendicularPolyPoint(
+    result: LSTriple,
     p0: LSTriple,
     p1: LSTriple,
     alfaPlus90: Float
-): LSTriple {
+) {
     val width = ((p0.w + p1.w) / 2.0)
     val x = (p0.x + p1.x) / 2.0 + width * sin(alfaPlus90)
     val y = (p0.y + p1.y) / 2.0 + width * cos(alfaPlus90)
-    return LSTriple(x.toFloat(), y.toFloat(), 1.0F)
+    result.x = x.toFloat()
+    result.y = y.toFloat()
 }
 
 private fun calculateSidesOfTriangle(
     p0: LSTriple,
     p1: LSTriple
-): Triple<Float,Float,Float>{
+): Triple<Float, Float, Float> {
     val a = p0.x - p1.x
     val b = p0.y - p1.y
     val c = sqrt(a.toDouble().pow(2.0) + b.toDouble().pow(2.0)).toFloat()
